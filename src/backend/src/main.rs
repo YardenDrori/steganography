4f6a7921f687d00ed9ab2b4e2c911cd09fbea972
YardenDrori <yarden.drori.yd@gmail.com>
2025-10-15 20:44:18 +0300

    added reading the payload and carrier checked if carrier is large enough for payload

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,8 +1,24 @@
-use image::ImageReader;
+use image::{self};
+use std::fs::{self};
 
 const IMAGE_FILE: &str = "../../examples/images/png_image.png";
+const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
 
-fn main() {
-    let frame = image::ImageReader::open(IMAGE_FILE). //there are like 30 unwrap options also it says in the docs to use "let img = ImageReader::open("myimage.png")?.decode()?;";
-    let dimensions = frame //broooooooooooooooooooooo wtf is this
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //read carrier file
+    let frame = image::open(IMAGE_FILE)?.into_rgba8();
+    let dimensions = frame.dimensions();
+    println!("{:?}", dimensions);
+
+    //check if carrier has capacity for payload
+    if fs::metadata(PAYLOAD)?.len() > dimensions.0 as u64 * dimensions.1 as u64 * 4 {
+        return Err("Payload is too large to fit in the carrier file".into());
+    }
+    //read payload
+    let payload = fs::read(PAYLOAD);
+
+    let pixels = frame.as_raw();
+    println!("{:?}", &pixels[0..3]);
+
+    Ok(())
 }
