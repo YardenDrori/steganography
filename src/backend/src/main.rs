4c9127cc65b99473d0f789aedb7f9624540ab1fd
Jordan <yarden.drori.yd@gmail.com>
2025-10-16 13:26:29 +0300

    Added buffer to read the payload data in chunks

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,12 +1,16 @@
 use image::{self};
-use std::fs::{self};
+use std::fs::{self, File};
+use std::io::{BufReader, Read};
 
-const IMAGE_FILE: &str = "../../examples/images/png_image.png";
+const CARRIER: &str = "../../examples/images/png_image.png";
 const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const CHUNK_SIZE: usize = 8 * 1024;
+    let mut buffer = [0; CHUNK_SIZE];
+
     //read carrier file
-    let frame = image::open(IMAGE_FILE)?.into_rgba8();
+    let frame = image::open(CARRIER)?.into_rgba8();
     let dimensions = frame.dimensions();
     println!("{:?}", dimensions);
 
@@ -14,8 +18,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if fs::metadata(PAYLOAD)?.len() > dimensions.0 as u64 * dimensions.1 as u64 * 4 {
         return Err("Payload is too large to fit in the carrier file".into());
     }
-    //read payload
-    let payload = fs::read(PAYLOAD);
+    //read payload as chunks
+    let mut payload = File::open(PAYLOAD)?;
+    //read file in chunks
+    loop {
+        let bytes_read = payload.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        //actual data for use in case near end of file to avoid reading leftovers from last pass
+        let chunk_data = &buffer[0..bytes_read];
+    }
 
     let pixels = frame.as_raw();
     println!("{:?}", &pixels[0..3]);
