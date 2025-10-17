6a198164c14a415e3ca368c9d7e6b9dc057922a9
Jordan <yarden.drori.yd@gmail.com>
2025-10-17 08:46:51 +0300

    fixed weird syntaxing

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -6,8 +6,9 @@ const CARRIER: &str = "../../examples/images/png_image.png";
 const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const CHUNK_SIZE: usize = 8 * 1024;
+    const CHUNK_SIZE: usize = 128 * 1024; //128 mb
     let mut buffer = [0; CHUNK_SIZE];
+    let mut buffer_new = [0; CHUNK_SIZE];
 
     //read carrier file
     let frame = image::open(CARRIER)?.into_rgba8();
@@ -18,20 +19,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if fs::metadata(PAYLOAD)?.len() > dimensions.0 as u64 * dimensions.1 as u64 * 4 {
         return Err("Payload is too large to fit in the carrier file".into());
     }
-    //read payload as chunks
+
+    //read frame into memory
+    let pixels = frame.as_raw();
+    println!("{:?}", &pixels[0..3]); //read payload as chunks 
     let mut payload = File::open(PAYLOAD)?;
-    //read file in chunks
     loop {
         let bytes_read = payload.read(&mut buffer)?;
         if bytes_read == 0 {
             break;
         }
-        //actual data for use in case near end of file to avoid reading leftovers from last pass
         let chunk_data = &buffer[0..bytes_read];
     }
-
-    let pixels = frame.as_raw();
-    println!("{:?}", &pixels[0..3]);
 
     Ok(())
 }
