555c5defd1c14e49dbd7e6254e73230dd0ef5c52
Jordan <yarden.drori.yd@gmail.com>
2025-10-17 09:30:14 +0300

    changed buffer to vec alongside other misc changes

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,4 +1,4 @@
-use image::{self};
+use image::{self, ImageBuffer};
 use std::fs::{self, File};
 use std::io::{BufReader, Read};
 
@@ -6,19 +6,18 @@ const CARRIER: &str = "../../examples/images/png_image.png";
 const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const CHUNK_SIZE: usize = 128 * 1024; //128 mb
-    let mut buffer = [0; CHUNK_SIZE];
-    let mut buffer_new = [0; CHUNK_SIZE];
-
     //read carrier file
     let frame = image::open(CARRIER)?.into_rgba8();
     let dimensions = frame.dimensions();
     println!("{:?}", dimensions);
+    let mut new_image = ImageBuffer::new(dimensions.0, dimensions.1);
 
     //check if carrier has capacity for payload
     if fs::metadata(PAYLOAD)?.len() > dimensions.0 as u64 * dimensions.1 as u64 * 4 {
         return Err("Payload is too large to fit in the carrier file".into());
     }
+    let chunk_size = dimensions.0 * dimensions.1 * 4;
+    let mut buffer = vec![0; chunk_size as usize];
 
     //read frame into memory
     let pixels = frame.as_raw();
