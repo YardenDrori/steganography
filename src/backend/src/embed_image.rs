81ffe9f82ff4a823220245373b7508687a6c636a
Jordan <yarden.drori.yd@gmail.com>
2025-10-18 22:10:19 +0300

    started work on modifying image embedding to work for single video frames

--- src/backend/src/embed_image.rs
+++ src/backend/src/embed_image.rs
@@ -3,29 +3,38 @@ use std::fs::{self, File};
 use std::io::Read;
 
 pub fn embed_image(
-    carrier: &str,
-    payload: &str,
-) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, Box<dyn std::error::Error>> {
+    carrier: &mut [u8],
+    payload: &[u8],
+    width: u32,
+    height: u32,
+    channels: u8,
+) -> Result<usize, Box<dyn std::error::Error>> {
     //read carrier file
-    let frame = image::open(carrier)?.into_rgba8();
-    let dimensions = frame.dimensions();
-    println!("{:?}", dimensions);
+    // let frame = image::open(carrier)?.into_rgba8();
+    // let dimensions = frame.dimensions();
+    // println!("{:?}", dimensions);
+    //
+    // //we redyce by 32 as we mark the first 32 bits (1 byte) for the size of the payload to know how much of
+    // //the file we need to read incase the payload is smaller then the carrier
+    // let chunk_size = dimensions.0 * dimensions.1 * 4 - 32;
+    // //check if carrier has capacity for payload
+    // if fs::metadata(payload)?.len() > chunk_size as u64 {
+    //     return Err("Payload is too large to fit in the carrier file".into());
+    // }
+    // //buffer is same size as image to make one bugger fit one image exactly
+    // let mut buffer = vec![0; chunk_size as usize];
+    //
+    // //read frame into memory
+    // let mut pixels = frame.into_raw();
+    // println!("{:?}\n", &pixels[0..8]);
+    // let mut payload = File::open(payload)?;
+    // let bytes_read = payload.read(&mut buffer)?;
 
-    //we redyce by 64 as we mark the first 64 bits (2 bytes) for the size of the payload to know how much of
-    //the file we need to read incase the payload is smaller then the carrier
-    let chunk_size = dimensions.0 * dimensions.1 * 4 - 64;
-    //check if carrier has capacity for payload
-    if fs::metadata(payload)?.len() > chunk_size as u64 {
-        return Err("Payload is too large to fit in the carrier file".into());
+    //insert patload byte size
+    let payload_len = payload.len(); //in bytes
+    for i in 0..32 {
+        carrier[i] = carrier[i] & 0xFE; //set LSB to 0
     }
-    //buffer is same size as image to make one bugger fit one image exactly
-    let mut buffer = vec![0; chunk_size as usize];
-
-    //read frame into memory
-    let mut pixels = frame.into_raw();
-    println!("{:?}\n", &pixels[0..8]);
-    let mut payload = File::open(payload)?;
-    let bytes_read = payload.read(&mut buffer)?;
 
     //embed payload
     let chunk_data = &mut buffer[0..bytes_read];
@@ -46,5 +55,5 @@ pub fn embed_image(
     //debug line to save frame
     output_image.save("../../output/stego_files/stego_file.png")?;
 
-    Ok(output_image)
+    Ok()
 }
