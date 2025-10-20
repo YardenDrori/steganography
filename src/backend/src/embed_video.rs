de6f84754401610964d1e7a8f95083f00942876e
Random Internet Person <112799108+YardenDrori@users.noreply.github.com>
2025-10-20 17:52:54 +0300

    small tweaks

--- src/backend/src/embed_video.rs
+++ src/backend/src/embed_video.rs
@@ -1,6 +1,12 @@
+use ffmpeg_next as ffmpeg;
+
 fn embed_video(
     carrier_location: &str,
     payload_location: &str,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    Ok("ROCK AND STONE!".to_string())
+    ffmpeg::init()?;
+
+    //input context - handle for the video
+    let mut ictx = ffmpeg::format::input(carrier_location)?;
+    ictx.streams().best(Type::Video);
 }
