f9ba584c0635e8936a6533dda9b29a6fd92283e9
Jordan <yarden.drori.yd@gmail.com>
2025-10-21 22:37:14 +0300

    BRO THE STEGANOGRAPHY IN AND OUT WORK HURRAY
    i can finally go to sleep =D zzz

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,10 +1,15 @@
 mod embed_image;
 mod embed_video;
 mod extract_image;
+mod extract_video;
 
 const CARRIER: &str = "../../examples/videos_lossless/chicken_jockey.mkv";
 const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
+const STEG_FILE: &str = "../../output/steg_files/output.mkv";
+const EXTRACTED_PAYLOAD: &str = "../../output/payloads/output";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}", embed_video::embed_video(CARRIER, PAYLOAD)?);
+    embed_video::embed_video(CARRIER, PAYLOAD, STEG_FILE)?;
+    extract_video::extract_video(STEG_FILE, EXTRACTED_PAYLOAD)?;
     Ok(())
 }
