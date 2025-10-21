d402ae5abefe7a78c7f7d68329bf1c6e792e76b7
YardenDrori <yarden.drori.yd@gmail.com>
2025-10-21 18:26:11 +0300

    video embedder supposedly working
    I can't confirm for sure as no extractor yet lmao gotta pray it works
    for now XD

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -5,5 +5,6 @@ mod extract_image;
 const CARRIER: &str = "../../examples/videos_lossless/chicken_jockey.mkv";
 const PAYLOAD: &str = "../../examples/hideable_files/bee_movie_script.txt";
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", embed_video::embed_video(CARRIER, PAYLOAD)?);
     Ok(())
 }
