65ea0a885e2de53faef457bbc3fc50f0cf8e822e
YardenDrori <yarden.drori.yd@gmail.com>
2025-10-24 20:10:36 +0300

    did a bunch if prep work for backend and added docker + migrations to easily set up database as well as the User struct

--- src/backend/src/steganography/embed_video.rs
+++ src/backend/src/steganography/embed_video.rs
@@ -1,4 +1,4 @@
-use crate::embed_image;
+use crate::steganography::embed_image;
 use std::fs::File;
 use std::io::Read;
 
--- src/backend/src/steganography/extract_video.rs
+++ src/backend/src/steganography/extract_video.rs
@@ -1,4 +1,4 @@
-use crate::extract_image;
+use crate::steganography::extract_image;
 use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
