70b8817006c2268c2d5fa218cbaff4625508ff3c
Jordan <yarden.drori.yd@gmail.com>
2025-12-04 19:42:23 +0200

    auth_service now loads public key for AdminOrInternal extractor

--- src/backend/auth_service/src/app_state.rs
+++ src/backend/auth_service/src/app_state.rs
@@ -4,8 +4,15 @@ use sqlx::{Pool, Postgres};
 pub struct AppState {
     pub pool: Pool<Postgres>,
     pub jwt_private_key: String,
+    pub jwt_public_key: String,
     pub internal_api_key: String,
     pub user_service_url: String,
+}
+
+impl shared_global::auth::jwt::HasJwtPublicKey for AppState {
+    fn jwt_public_key(&self) -> String {
+        self.jwt_public_key.to_string()
+    }
 }
 
 impl shared_global::auth::internal::HasInternalApiKey for AppState {
