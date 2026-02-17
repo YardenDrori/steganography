0db0d93daf77d47561ea5a16bf06daba14640a60
Jordan <yarden.drori.yd@gmail.com>
2026-02-17 17:29:37 +0200

    updated prep_dbs script

--- src/backend/prep_dbs.sh
+++ src/backend/prep_dbs.sh
@@ -1,6 +1,13 @@
+docker-compose up auth_postgres -d
+docker-compose up user_postgres -d
+docker-compose up files_postgres -d
 cd auth_service
 sqlx migrate run
 cargo sqlx prepare
 cd ../user_service
 sqlx migrate run
 cargo sqlx prepare
+cd ../files_service
+sqlx migrate run
+cargo sqlx prepare
+cd ..
