05b0f43f3c6517e8880be303ce6e4a87696bcfc5
Jordan <yarden.drori.yd@gmail.com>
2026-02-18 11:43:37 +0200

    updated app_state for auth_service to store arc of the eureka cofigs

--- src/backend/auth_service/src/app_state.rs
+++ src/backend/auth_service/src/app_state.rs
@@ -1,3 +1,6 @@
+use std::sync::{Arc, RwLock};
+
+use shared_global::eureka::EurekaConfig;
 use sqlx::{Pool, Postgres};
 
 #[derive(Clone)]
@@ -5,7 +8,7 @@ pub struct AppState {
     pub pool: Pool<Postgres>,
     pub jwt_private_key: String,
     pub jwt_public_key: String,
-    pub user_service_url: String,
+    pub eureka_config: Arc<RwLock<EurekaConfig>>,
 }
 
 impl shared_global::auth::jwt::HasJwtPublicKey for AppState {
