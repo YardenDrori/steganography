aa556c79caf4ae21b79108ae4178fe4ec8cb8f09
Jordan <yarden.drori.yd@gmail.com>
2026-02-20 17:51:47 +0200

    i hate react

---
+++ src/frontend/src/context/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useContext } from "react";
+
+type User = {
+  id: number;
+  user_name: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string | null;
+  is_male: boolean | null;
+  is_verified: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+type AuthContextType = {
+  accessToken: string | null;
+  user: User | null;
+  setAccessToken: (token: string | null) => void;
+  setUser: (user: object | null) => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+export function AuthProvider(props: { children: React.ReactNode }) {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+}
