00ba4388c508d4f0b1b8d8e4ffa5beda234b696e
Jordan <yarden.drori.yd@gmail.com>
2026-02-21 19:37:39 +0200

    made dashboard file

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={LoginPage()} />
-        <Route path="/register" element={RegisterPage()} />
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/login" element=<LoginPage /> />
+        <Route path="/register" element=<RegisterPage /> />
+        <Route path="/dashboard" element=<Dashboard /> />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <p>dashborad</p>;
+}
+
+export default Dashboard;
