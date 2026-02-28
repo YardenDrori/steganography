76cb89bd2df28776cde1ab24fb4525fb59d50013
Jordan <yarden.drori.yd@gmail.com>
2026-02-28 11:59:31 +0200

    refactored app state to add paramaters for minio and implemented trait hasjwtpublickey for extractors to work

--- src/backend/files_service/src/app_state.rs
+++ src/backend/files_service/src/app_state.rs
@@ -1,9 +1,21 @@
+use s3::Bucket;
 use shared_global::eureka::EurekaConfig;
-use sqlx::{Postgres, Pool};
+use sqlx::{Pool, Postgres};
 use std::sync::{Arc, RwLock};
 
 #[derive(Clone)]
 pub struct AppState {
     pub pool: Pool<Postgres>,
     pub eureka_config: Arc<RwLock<EurekaConfig>>,
+    pub bucket: Bucket,
+}
+
+impl shared_global::auth::jwt::HasJwtPublicKey for AppState {
+    fn jwt_public_key(&self) -> String {
+        self.eureka_config
+            .read()
+            .unwrap()
+            .jwt_public_key
+            .to_string()
+    }
 }
