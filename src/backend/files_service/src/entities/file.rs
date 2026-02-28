0c60a5a16f3ac826ba8b98424c33db472af92d2f
Jordan <yarden.drori.yd@gmail.com>
2026-02-28 12:09:08 +0200

    added files_service entity for postgres

---
+++ src/backend/files_service/src/entities/file.rs
@@ -0,0 +1,10 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Clone)]
+pub struct FileEntity {
+    pub id: i64,
+    pub user_id: i64,
+    pub filename: String,
+    pub object_key: String,
+    pub created_at: DateTime<Utc>,
+}
