27dff9561d893dacd9f024021e4eb64888917a5c
Jordan <yarden.drori.yd@gmail.com>
2026-03-01 15:20:25 +0200

    creates minio bucket if it doesnt exist

--- src/backend/files_service/src/main.rs
+++ src/backend/files_service/src/main.rs
@@ -70,6 +70,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("failed to initialize bucket. error")
         .with_path_style();
 
+    // Create bucket if it doesn't exist
+    match bucket.create_bucket().await {
+        Ok(_) => tracing::info!("Bucket '{}' created", bucket_name),
+        Err(s3::error::S3Error::HttpFailWithBody(409, _)) => {
+            tracing::info!("Bucket '{}' already exists", bucket_name)
+        }
+        Err(e) => panic!("Failed to create bucket '{}': {:?}", bucket_name, e),
+    }
+
     // Create app state
     let app_state = AppState {
         pool: pool,
