c89f9ba7aa00aaa499b6857c689563098aa76ce9
Jordan <yarden.drori.yd@gmail.com>
2026-03-01 14:56:59 +0200

    wired routes in main

--- src/backend/files_service/src/main.rs
+++ src/backend/files_service/src/main.rs
@@ -1,12 +1,10 @@
 use crate::app_state::AppState;
-use crate::routes::post_files;
-use crate::services::files_service;
 use std::sync::{Arc, RwLock};
 // use crate::routes::{auth, delete_users, patch_users, post_users, sync};
 use axum::routing::{delete, get, patch, post};
 use axum::Router;
 use s3::creds::Credentials;
-use s3::{region, Bucket, Region};
+use s3::{Bucket, Region};
 // use routes::get_users;
 use shared_global::db::postgres::create_pool;
 use tower_http::trace::TraceLayer;
@@ -84,9 +82,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Build router
     let app = Router::new()
         .layer(TraceLayer::new_for_http())
-        .route("/files/initiate", post(post_files::initiate))
-        .route("/files/upload-chunk", post(post_files::upload_chunk))
-        .route("/file/complete", post(post_files::complete_upload))
+        .route("/files/initiate", post(routes::post_files::initiate))
+        .route(
+            "/files/upload-chunk",
+            post(routes::post_files::upload_chunk),
+        )
+        .route("/file/complete", post(routes::post_files::complete_upload))
+        .route("/files/me", get(routes::get_files::get_files_for_user))
+        .route("/files/:id", get(routes::get_files::get_download_url))
+        .route("/files/:id", patch(routes::patch_files::rename_file))
+        .route("/files/:id", delete(routes::delete_files::delete_file))
         .with_state(app_state);
 
     // Spawn heartbeat task
