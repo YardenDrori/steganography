2cc6e242a436f308cd569516175ca4a82bfa6201
Jordan <yarden.drori.yd@gmail.com>
2026-03-02 09:51:26 +0200

    added bare bones error handling file
    expand as needed

--- src/backend/steganography_service/src/errors/steg_service_error.rs
+++ src/backend/steganography_service/src/errors/steg_service_error.rs
@@ -6,31 +6,19 @@ use axum::{
 use shared_global::errors::ErrorBody;
 
 #[derive(Debug)]
-pub enum UserServiceError {
-    // EmailAlreadyExists,
-    // UsernameAlreadyExists,
-    // DatabaseError(sqlx::Error),
-    // HashingError(argon2::password_hash::Error),
-    // InvalidCredentials,
-    // JwtError(jsonwebtoken::errors::Error),
-    // ExternalServiceError(String), // For HTTP calls to other microservices
+pub enum StegServiceError {
+    ExternalServiceError(String),
 }
 
-// impl IntoResponse for UserServiceError {
-//     fn into_response(self) -> Response {
-//         let (status, message) = match self {
-//             Self::EmailAlreadyExists => (StatusCode::CONFLICT, "Email already exists"),
-//             Self::UsernameAlreadyExists => (StatusCode::CONFLICT, "Username already exists"),
-//             Self::InvalidCredentials => (StatusCode::UNAUTHORIZED, "Invalid credentials"),
-//             Self::DatabaseError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-//             Self::HashingError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-//             Self::JwtError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-//             Self::ExternalServiceError(_) => (
-//                 StatusCode::INTERNAL_SERVER_ERROR,
-//                 "Failed to sync with external service",
-//             ),
-//         };
-//
-//         (status, Json(ErrorBody::new(message))).into_response()
-//     }
-// }
+impl IntoResponse for StegServiceError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            Self::ExternalServiceError(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to sync with external service",
+            ),
+        };
+
+        (status, Json(ErrorBody::new(message))).into_response()
+    }
+}
