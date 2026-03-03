38bdd67289b4f45b2ad081cafdbdda2d95c08dc2
Jordan <yarden.drori.yd@gmail.com>
2026-03-03 21:55:21 +0200

    added new error for ffmpeg bs

--- src/backend/steganography_service/src/errors/steg_service_error.rs
+++ src/backend/steganography_service/src/errors/steg_service_error.rs
@@ -11,11 +11,13 @@ pub enum StegServiceError {
     ParsingError,
     FileError,
     EurekaConfigError,
+    FfmpegError(ffmpeg_next::Error),
 }
 
 impl IntoResponse for StegServiceError {
     fn into_response(self) -> Response {
         let (status, message) = match self {
+            Self::FfmpegError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
             Self::EurekaConfigError => (StatusCode::BAD_GATEWAY, "Internal server error"),
             Self::ParsingError => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
             Self::FileError => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
