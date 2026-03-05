9ab88addd10a7fbc8cf28dcc11b60f85c3ea742f
Jordan <yarden.drori.yd@gmail.com>
2026-03-05 12:08:08 +0200

    refactored to expect input to be of type [u8; 16] as it makes plumbing the data simpler

--- src/backend/steganography_service/src/services/qim.rs
+++ src/backend/steganography_service/src/services/qim.rs
@@ -1,26 +1,23 @@
 use crate::errors::steg_service_error::StegServiceError;
 
 #[rustfmt::skip]
-pub const ZIGZAG: [(usize, usize); 16] = [
-    (0, 0), (0, 1), (1, 0), (2, 0),
-    (1, 1), (0, 2), (0, 3), (1, 2),
-    (2, 1), (3, 0), (3, 1), (2, 2),
-    (1, 3), (2, 3), (3, 2), (3, 3),
+pub const ZIGZAG: [usize ; 16] = [
+0, 1, 4, 8, 5, 2, 3, 6, 9, 12, 13, 10, 7, 11, 14, 15,
 ];
 
 // bit=true  → nearest multiple of delta
 // bit=false → nearest odd multiple of delta/2
 pub fn qim_embed(
-    mut block: [[f64; 4]; 4],
+    mut block: [f64; 16],
     bit: bool,
     delta: u8,
     zigzag_index: usize,
-) -> Result<[[f64; 4]; 4], StegServiceError> {
+) -> Result<[f64; 16], StegServiceError> {
     let delta_float = delta as f64;
-    let (x, y) = ZIGZAG[zigzag_index];
-    let coeff = block[x][y];
+    let flat_index = ZIGZAG[zigzag_index];
+    let coeff = block[flat_index];
 
-    block[x][y] = if bit {
+    block[flat_index] = if bit {
         (coeff / delta_float).round() * delta_float
     } else {
         ((coeff / delta_float - 0.5).round() + 0.5) * delta_float
@@ -30,10 +27,10 @@ pub fn qim_embed(
 }
 
 // Reads the embedded bit from a DCT coefficient.
-pub fn qim_extract(block: [[f64; 4]; 4], delta: u8, zigzag_index: usize) -> bool {
+pub fn qim_extract(block: [f64; 16], delta: u8, zigzag_index: usize) -> bool {
     let delta_float = delta as f64;
-    let (x, y) = ZIGZAG[zigzag_index];
-    let coeff = block[x][y];
+    let flat_index = ZIGZAG[zigzag_index];
+    let coeff = block[flat_index];
 
     let dist_true = (coeff - (coeff / delta_float).round() * delta_float).abs();
     let dist_false = (coeff - ((coeff / delta_float - 0.5).round() + 0.5) * delta_float).abs();
