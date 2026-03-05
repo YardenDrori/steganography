6618ac647183323377bc82ad54cacaa435e183eb
Jordan <yarden.drori.yd@gmail.com>
2026-03-05 11:01:07 +0200

    fixed DCT to use an array instead of a matrix

--- src/backend/steganography_service/src/services/dct.rs
+++ src/backend/steganography_service/src/services/dct.rs
@@ -1,8 +1,7 @@
 use std::f64::consts::PI;
 
-pub fn dct_ii(pixels: [[u8; 4]; 4]) -> [[f64; 4]; 4] {
-    let mut coefficients = [[0.0f64; 4]; 4];
-
+pub fn dct_ii(pixels: &[u8; 16]) -> [f64; 16] {
+    let mut coefficients = [0.0f64; 16];
     //to understand this weird ass forumula what i recommend is going to desmos and pasting these
     //values as 1 function and 4 positions
     //y=\cos\left(kx\right)
@@ -18,13 +17,13 @@ pub fn dct_ii(pixels: [[u8; 4]; 4]) -> [[f64; 4]; 4] {
             let mut sum = 0.0f64;
             for i_x in 0..4 {
                 for i_y in 0..4 {
-                    let curr_pixel = pixels[i_x][i_y] as f64;
+                    let curr_pixel = pixels[i_x * 4 + i_y] as f64; // was pixels[i_x][i_y]
                     let angle_x = k_x as f64 * (2.0 * i_x as f64 + 1.0) * PI / 8.0;
                     let angle_y = k_y as f64 * (2.0 * i_y as f64 + 1.0) * PI / 8.0;
                     sum += curr_pixel * angle_x.cos() * angle_y.cos();
                 }
             }
-            coefficients[k_x][k_y] = sum;
+            coefficients[k_x * 4 + k_y] = sum; // was coefficients[k_x][k_y]
         }
     }
     coefficients
@@ -40,15 +39,14 @@ numbers and it is happening so this code handles it lol we also clamp and round 
 that float point errors dont add up and that if we hadd a dct value of 100 and we made it 102 the
 cascading change on all pixels the dct influenced wont push them to a negative value which will
 be interperted as 255 when converting them back to u8*/
-pub fn idct_ii(coefficients: [[f64; 4]; 4]) -> [[u8; 4]; 4] {
-    let mut pixels = [[0u8; 4]; 4];
-
+pub fn idct_ii(coefficients: &[f64; 16]) -> [u8; 16] {
+    let mut pixels = [0u8; 16];
     for i_x in 0..4 {
         for i_y in 0..4 {
             let mut sum = 0.0f64;
             for k_x in 0..4 {
                 for k_y in 0..4 {
-                    let curr_coefficient = coefficients[k_x][k_y];
+                    let curr_coefficient = coefficients[k_x * 4 + k_y]; // was coefficients[k_x][k_y]
                     let scale_x = if k_x == 0 { 0.25 } else { 0.5 };
                     let scale_y = if k_y == 0 { 0.25 } else { 0.5 };
                     let angle_x = k_x as f64 * (2.0 * i_x as f64 + 1.0) * PI / 8.0;
@@ -56,7 +54,7 @@ pub fn idct_ii(coefficients: [[f64; 4]; 4]) -> [[u8; 4]; 4] {
                     sum += curr_coefficient * scale_x * angle_x.cos() * scale_y * angle_y.cos();
                 }
             }
-            pixels[i_x][i_y] = sum.round().clamp(0.0, 255.0) as u8;
+            pixels[i_x * 4 + i_y] = sum.round().clamp(0.0, 255.0) as u8; // was pixels[i_x][i_y]
         }
     }
     pixels
