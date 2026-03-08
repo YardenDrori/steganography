01ef0f54ebed49289c9dd76369c036cc38031951
Jordan <yarden.drori.yd@gmail.com>
2026-03-08 21:05:25 +0200

    new error variant for insufficient capacity in carrier for payload

--- src/backend/steganography_service/src/errors/steg_service_error.rs
+++ src/backend/steganography_service/src/errors/steg_service_error.rs
@@ -15,11 +15,16 @@ pub enum StegServiceError {
     FfmpegError(ffmpeg_next::Error),
     Other(String),
     UnsupportedCodec,
+    InsufficientCapacity,
 }
 
 impl IntoResponse for StegServiceError {
     fn into_response(self) -> Response {
         let (status, message) = match self {
+            Self::InsufficientCapacity => (
+                StatusCode::BAD_REQUEST,
+                "Payload too big for selected carrier",
+            ),
             Self::InvalidPayload => (StatusCode::BAD_REQUEST, "Invalid payload"),
             Self::UnsupportedCodec => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
             Self::FfmpegError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
