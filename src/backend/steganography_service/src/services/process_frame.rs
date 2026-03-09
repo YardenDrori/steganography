6b6bc3bf44047d379fbad5e4793f4bb9c0b47d72
Jordan <yarden.drori.yd@gmail.com>
2026-03-09 10:57:25 +0200

    made calculating resolution its own method to allow calling independantly

--- src/backend/steganography_service/src/services/process_frame.rs
+++ src/backend/steganography_service/src/services/process_frame.rs
@@ -1,3 +1,4 @@
+use ffmpeg_next::Codec;
 use ffmpeg_next::format::Pixel;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
@@ -10,6 +11,33 @@ pub struct BufferGeneric {
     pub buffer: [u8; 1028],
     pub bit_index: usize,
     pub bits_read: usize,
+}
+
+const Y_PLANE: usize = 0;
+const CB_PLANE: usize = 1;
+const CR_PLANE: usize = 2;
+
+pub fn find_dimensions_for_codec(
+    frame: &mut ffmpeg_next::frame::Video,
+    configs: &EmbedConfigs,
+) -> Result<(u32, u32, u32, u32, u32, u32), StegServiceError> {
+    if configs.channels_to_embed.yuv.is_some() {
+        // (y_width_div, y_height_div, cb_width_div, cb_height_div, cr_width_div, cr_height_div)
+        let (y_width_div, y_height_div, cb_width_div, cb_height_div, cr_width_div, cr_height_div): (
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+        u32,
+    ) = match frame.format() {
+        Pixel::YUV420P => return Ok((frame.width()/1, frame.height()/1, frame.width()/2, frame.height()/2, frame.width()/2, frame.height()/2)),
+        Pixel::YUV422P => return Ok((frame.width()/1, frame.height()/1, frame.width()/2, frame.height()/1, frame.width()/2, frame.height()/1)),
+        Pixel::YUV444P => return Ok((frame.width()/1, frame.height()/1, frame.width()/1, frame.height()/1, frame.width()/1, frame.height()/1)),
+        _ => return Err(StegServiceError::UnsupportedCodec),
+    };
+    }
+    Err(StegServiceError::UnsupportedCodec)
 }
 
 pub fn process_frame<F>(
@@ -28,49 +56,20 @@ where
         u32,
     ) -> Result<(), StegServiceError>,
 {
-    const Y_PLANE: usize = 0;
-    const CB_PLANE: usize = 1;
-    const CR_PLANE: usize = 2;
-
-    //allows to add more codecs, eng RGB also allows prioritizing best channel to embed in by
+    //allows to add more codecs, eg RGB also allows prioritizing best channel to embed in by
     //changing order of if statements for sensible defaults
     if let Some(yuv) = &configs.channels_to_embed.yuv {
-        // (y_width_div, y_height_div, cb_width_div, cb_height_div, cr_width_div, cr_height_div)
-        let (y_width_div, y_height_div, cb_width_div, cb_height_div, cr_width_div, cr_height_div): (u32, u32, u32, u32, u32, u32) = match frame.format() {
-            Pixel::YUV420P => (1, 1, 2, 2, 2, 2),
-            Pixel::YUV422P => (1, 1, 2, 1, 1, 1),
-            Pixel::YUV444P => (1, 1, 1, 1, 1, 1),
-            _ => return Err(StegServiceError::UnsupportedCodec),
-        };
+        let (y_wdith, y_height, u_width, u_height, v_width, v_height) =
+            find_dimensions_for_codec(frame, configs)?;
+
         if yuv.y {
-            channel_method(
-                frame,
-                configs,
-                buffer,
-                Y_PLANE,
-                frame.width() / y_width_div,
-                frame.height() / y_height_div,
-            )?;
+            channel_method(frame, configs, buffer, Y_PLANE, y_wdith, y_height)?;
         }
         if yuv.cb {
-            channel_method(
-                frame,
-                configs,
-                buffer,
-                CB_PLANE,
-                frame.width() / cb_width_div,
-                frame.height() / cb_height_div,
-            )?;
+            channel_method(frame, configs, buffer, CB_PLANE, u_width, u_height)?;
         }
         if yuv.cr {
-            channel_method(
-                frame,
-                configs,
-                buffer,
-                CR_PLANE,
-                frame.width() / cr_width_div,
-                frame.height() / cr_height_div,
-            )?;
+            channel_method(frame, configs, buffer, CR_PLANE, v_width, v_height)?;
         }
     } else {
         return Err(StegServiceError::UnsupportedCodec);
