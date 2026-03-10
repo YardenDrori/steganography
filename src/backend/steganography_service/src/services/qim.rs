1fead7df5d83e87159b6dd4883e5b33d0c0ba7e1
Jordan <yarden.drori.yd@gmail.com>
2026-03-10 16:47:42 +0200

    updated qim methods to work on a single value for stdm

--- src/backend/steganography_service/src/services/qim.rs
+++ src/backend/steganography_service/src/services/qim.rs
@@ -5,25 +5,19 @@ pub const ZIGZAG: [usize ; 16] = [
 
 // bit=true  → nearest multiple of delta
 // bit=false → nearest odd multiple of delta/2
-pub fn qim_embed(mut block: [f64; 16], bit: bool, delta: u8, zigzag_index: usize) -> [f64; 16] {
+pub fn qim_embed(coeff: f64, bit: bool, delta: u8) -> f64 {
     let delta_float = delta as f64;
-    let flat_index = ZIGZAG[zigzag_index];
-    let coeff = block[flat_index];
 
-    block[flat_index] = if bit {
-        (coeff / delta_float).round() * delta_float
+    if bit {
+        return (coeff / delta_float).round() * delta_float;
     } else {
-        ((coeff / delta_float - 0.5).round() + 0.5) * delta_float
-    };
-
-    block
+        return ((coeff / delta_float - 0.5).round() + 0.5) * delta_float;
+    }
 }
 
 // Reads the embedded bit from a DCT coefficient.
-pub fn qim_extract(block: [f64; 16], delta: u8, zigzag_index: usize) -> bool {
+pub fn qim_extract(coeff: f64, delta: u8) -> bool {
     let delta_float = delta as f64;
-    let flat_index = ZIGZAG[zigzag_index];
-    let coeff = block[flat_index];
 
     let dist_true = (coeff - (coeff / delta_float).round() * delta_float).abs();
     let dist_false = (coeff - ((coeff / delta_float - 0.5).round() + 0.5) * delta_float).abs();
