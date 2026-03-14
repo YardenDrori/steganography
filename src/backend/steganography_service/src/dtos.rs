0d1165f3f58e389a23a6b09555547324e0d8cc6a
Jordan <yarden.drori.yd@gmail.com>
2026-03-14 16:06:16 +0200

    added extract handlers

--- src/backend/steganography_service/src/dtos.rs
+++ src/backend/steganography_service/src/dtos.rs
@@ -1,6 +1,14 @@
+use std::any::Any;
+
 use crate::{
     errors::steg_service_error::StegServiceError,
-    services::{spread_spectrum::spread_spectrum_embed, stdm::stdm_embed},
+    services::{
+        qim::{qim_embed, qim_extract_bit},
+        spread_spectrum::{
+            improved_spread_spectrum_embed, spread_spectrum_embed, spread_spectrum_extract,
+        },
+        stdm::{stdm_embed, stdm_extract},
+    },
 };
 use serde::{Deserialize, Serialize};
 pub use shared_global::dtos::UserResponse;
@@ -35,6 +43,30 @@ pub struct EmbedConfigs {
     pub method: EmbedMethods,
 }
 
+impl EmbedConfigs {
+    pub fn validate_configs(&self) -> Result<(), StegServiceError> {
+        if self.coefficients_per_bit < 1 {
+            return Err(StegServiceError::InvalidPayload);
+        }
+
+        let mut found_coeff = false;
+        for i in 0..16 {
+            if self.coefficients_to_embed[i] == true {
+                found_coeff = true;
+                break;
+            }
+        }
+        if !found_coeff {
+            return Err(StegServiceError::InvalidPayload);
+        }
+
+        if matches!(self.method, EmbedMethods::QIM) && self.coefficients_per_bit != 1 {
+            return Err(StegServiceError::InvalidPayload);
+        }
+        Ok(())
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum EmbedMethods {
     QIM,
@@ -55,7 +87,7 @@ impl EmbedMethods {
     ) -> Result<(), StegServiceError> {
         match self {
             EmbedMethods::QIM => {
-                qim::embed(todo!());
+                qim_embed(get_coeff, set_coeff, bit, delta)?;
                 Ok(())
             }
             EmbedMethods::STDM => {
@@ -81,12 +113,61 @@ impl EmbedMethods {
                 Ok(())
             }
             EmbedMethods::ISS => {
-                iss::embed(todo!());
+                improved_spread_spectrum_embed(
+                    get_coeff,
+                    set_coeff,
+                    coeff_count,
+                    seed.ok_or(StegServiceError::InvalidPayload)?,
+                    bit,
+                    delta,
+                )?;
                 Ok(())
             }
         }
     }
-    // pub fn extract()
+
+    pub fn extract(
+        self,
+        get_coeff: impl Fn(usize) -> Result<f64, StegServiceError>,
+        coeff_count: usize,
+        seed: Option<String>,
+        delta: u8,
+    ) -> Result<bool, StegServiceError> {
+        match self {
+            Self::QIM => {
+                let coeff = get_coeff(0)?;
+                let found_bit = qim_extract_bit(coeff, delta);
+                return Ok(found_bit);
+            }
+            Self::STDM => {
+                let found_bit = stdm_extract(
+                    get_coeff,
+                    coeff_count,
+                    seed.ok_or(StegServiceError::InvalidPayload)?,
+                    delta,
+                )?;
+                return Ok(found_bit);
+            }
+            //since the only difference between SS and ISS is the embed step both use the same
+            //extractor for decoding
+            Self::SS => {
+                let found_bit = spread_spectrum_extract(
+                    get_coeff,
+                    coeff_count,
+                    seed.ok_or(StegServiceError::InvalidPayload)?,
+                )?;
+                return Ok(found_bit);
+            }
+            Self::ISS => {
+                let found_bit = spread_spectrum_extract(
+                    get_coeff,
+                    coeff_count,
+                    seed.ok_or(StegServiceError::InvalidPayload)?,
+                )?;
+                return Ok(found_bit);
+            }
+        };
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
