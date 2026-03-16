09100d7303248c1176e114979d33aa4ee20001fb
Jordan <yarden.drori.yd@gmail.com>
2026-03-16 15:31:21 +0200

    finished method that returns the roots of a generator with a given length
    removed unused import

--- src/backend/steganography_service/src/services/rs_generator_vec.rs
+++ src/backend/steganography_service/src/services/rs_generator_vec.rs
@@ -1,7 +1,4 @@
-use crate::{
-    errors::steg_service_error::StegServiceError,
-    services::gause_field::{EXP_TABLE, poly_mult_vecs},
-};
+use crate::services::gause_field::{EXP_TABLE, poly_mult_vecs};
 
 //great name i know
 //for len = 2 we expect alpha1(a1)=0 a2=1
@@ -9,7 +6,7 @@ use crate::{
 //we then mult then and expect to get (x^2+2x+x+2) or x^2+3x+2 (gf math replaces + with xor) which we represent in the arr as such
 //arr[0] = 2 arr[1] = 3 arr[2] = 1
 //this is the intended result of the generator for len = 2 note the ret len is len+1
-pub fn generatae_generator(len: u8) -> Vec<u8>  {
+pub fn generatae_generator(len: u8) -> Vec<u8> {
     let mut generator: Vec<u8> = vec![1, 1];
 
     for i in 1..len {
@@ -23,5 +20,10 @@ pub fn generatae_generator(len: u8) -> Vec<u8>  {
     generator
 }
 
-
-puf fn get_roots_for_generator_with_len(len:u8) -> 
+pub fn get_roots_for_generator_with_len(len: u8) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::with_capacity(len as usize);
+    for i in 0..len {
+        result.push(EXP_TABLE[i as usize]);
+    }
+    result
+}
