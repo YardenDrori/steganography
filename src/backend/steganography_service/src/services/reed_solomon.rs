4b130cd2b66519396ee083e9a562470c848b92fe
Jordan <yarden.drori.yd@gmail.com>
2026-03-17 11:25:03 +0200

    added bible explanation at files top for algo explanation for future me

--- src/backend/steganography_service/src/services/reed_solomon.rs
+++ src/backend/steganography_service/src/services/reed_solomon.rs
@@ -1,3 +1,61 @@
+// the bible of reed solomon cause this algo is a bitch
+// we take x bytes we wanna encode pad them with y parity bytes for every 2 parity bytes we can
+// recover 1 error. now we use GF math here so plus and minus are both XOR we generate a table for
+// quick multiplication and division via a generator (2) which is a number that can beacome any
+// number in GF thanks to GF's special wrapping nature when a value goes abbove 255 (e.g
+// multiplication) it wraps back in a special way eg we have 0b10000000 * 2 we do a shift left and
+// if it overflows (which it does here) we do a xor with the GF version of a prime number (0x011D)
+// after we generate the tables it gets much easier lets say we have EXP_TABLE so EXP_TABLE[0] =
+// 2^0 EXP_TABLE[1] = 2^1 and so on note this is GF power of so we multiply with GF math the reason
+// we need GF math is so that a byte will wrap in a usefull manner and that it will never have a
+// decimal point anywho to multiply 4 with 8 for instance we take the position of 4 and eight in
+// the exp table (via a log table) so for 4 its 2 and for 8 its 3 we add up their positions (with
+// regular math here) which
+// comes up to 5 and look and EXP_TABLE[5] for the result coming up to 32 (it is consistent
+// with regular multiplication untill it wraps) for division we instead of adding up the positions
+// we subtract them (again with regular math) if we overflow or underflow we add or subtract 255 (with regular math) untill we are between 0-255
+// again
+//
+// to encode we use the generator element (2) to make a generator polynomal so if we wanted 5
+// parity bytes the generator would be g(x) = (x-2^0)*(x-2^1)*(x-2^2)*(x-2^3)*(x-2^4) multiplying
+// them will give us a formula that plugging any value from 2^0 to 2^4 makes g(x) = 0 this is
+// usefull for for reasons that will be explained later, after we have the generator we take the
+// original message pad a zero for each parity byte we said we wanted and divide that with the
+// generator using long division we take whatever remainder is left which is gauaranteed to be at
+// most as big as parity bytes because math is cool like that, we replace the zeros with the
+// remainders this gives us the benifit of division with the generator now giving us zero which
+// will be very usefull for decoding we are now done with the encoding step
+//
+// for decoding we take a vec of Syndromes aka how and where are the errors we find them by taking
+// the message we received on our end and plugging j generator roots in it where j is how many
+// parity bytes we added so for instance if the message on our end is m=1+2x+3x^2+4x^3 meaning first
+// byte is 1 second is 2 third is 3 and so on and we had 2 parity bytes we plug 2^0 and 2^1 in the
+// mplace of x which will produce 2 syndromes for us if all syndromes's value is 0 we're done this
+// is why we wanted division to with the generator in earlier steps to equate 0 cause the error
+// formula is something like this e(x) = message*x + error*x so now if we plug in one of the values
+// from the generator roots e.g 2^0 e(1) = message*1 + error*1 thanks to how we built the message
+// we know that regardless of what the mssage was it is now 0 leaving us with e(1) = error*1 which
+// is very usefull for us we can expand error to be Y*X where Y is how off the byte is from
+// what it was supposed to be and X is in which recieved byte the error is this doesnt tell us
+// anything at all but these are the components of the error if we had more than 1 error the
+// formula would be Y1*X1 Y2*X2 so we can make a polynom of all the errors called
+// e(x) = Y1*X^1 + Y2*X^2 + ... Yn*X^n
+// where n is how many errors we had during transmission this is why every 2 parity bytes can fix 1
+// error because every error is made of amount and position and each parity byte will allow us to
+// recover 1 of those for reasons that will be explained later
+// in the encoding step we represented each byte's index with a power of x so we stick to that
+// pattern here if the error is in byte 4 than the err is Y4*X^4
+// the genric formula is
+// e = Y0*X^0 + Y1*X^1 + ... Yi*X^i
+//
+//
+//
+//
+//
+//
+//
+//
+//
 use std::{
     fs::File,
     io::{BufReader, BufWriter, Read, Write},
@@ -7,12 +65,15 @@ use std::{
 use crate::{
     dtos::EmbedConfigs,
     errors::steg_service_error::StegServiceError,
-    services::{gause_field::poly_div_remainder_vecs, rs_generator_vec::generatae_generator},
+    services::{
+        gause_field::{EXP_TABLE, poly_div_remainder_vecs, poly_mult},
+        rs_generator_vec::{generatae_generator, get_roots_for_generator_with_len},
+    },
 };
 
 pub fn reed_solomon_encode(
     payload_path: PathBuf,
-    configs: &EmbedConfigs,
+    configs: EmbedConfigs,
 ) -> Result<(), StegServiceError> {
     let output_path = format!("{}_encoded", &payload_path.display());
 
@@ -118,7 +179,31 @@ pub fn reed_solomon_decode(
     Ok(())
 }
 
-fn decode_chunk(chunk: &mut Vec<u8>, generator_len: u8) -> Result<(), StegServiceError> {
+//ret value of false means there were errors which RS was unable to fix
+fn decode_chunk(chunk: &mut Vec<u8>, generator_len: u8) -> Result<bool, StegServiceError> {
+    let generator_roots = get_roots_for_generator_with_len(generator_len);
+
+    let mut syndromes: Vec<u8> = Vec::with_capacity(generator_len as usize);
+
+    let mut found_error = false;
+    for i in 0..generator_len {
+        let mut sum = 0;
+        let curr_root = generator_roots[i as usize];
+        let mut adjusted_cur_root = 1;
+        for j in 0..chunk.len() {
+            sum ^= poly_mult(chunk[j], adjusted_cur_root);
+
+            adjusted_cur_root = poly_mult(curr_root, adjusted_cur_root);
+        }
+        syndromes.push(sum);
+        if sum != 0 {
+            found_error = true;
+        }
+    }
+    if !found_error {
+        return Ok(true);
+    }
+
     todo!()
 }
 
