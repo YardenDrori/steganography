d41c5ebaef6f85561760ea17bbb689c9a60a1fe3
Jordan <yarden.drori.yd@gmail.com>
2026-03-26 08:33:37 +0200

    temp disable rs

--- src/backend/steganography_service/src/routes/embed_video.rs
+++ src/backend/steganography_service/src/routes/embed_video.rs
@@ -2,7 +2,7 @@ use crate::{
     app_state::AppState,
     dtos::EmbedFileRequest,
     errors::steg_service_error::StegServiceError,
-    services::{embed_video::embed, files_client, reed_solomon::reed_solomon_encode},
+    services::{embed_video::embed, files_client},
 };
 use axum::{Json, extract::State, http::StatusCode};
 use files_dtos::FileResponse;
