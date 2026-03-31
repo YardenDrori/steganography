4873c16e8cf4e167ac2dc86f1a2a8b6187e3ab49
YardenDrori <yarden.drori.yd@gmail.com>
2026-03-31 12:18:10 +0300

    finished berelekamp massey (fr? like ive been working on understanding this for like a week now im so fucking happy! XD)
    thank god!
    and to think all this was for 80 lines of code 😭
    that's what i get for wanting to understand the math i guess

--- src/backend/steganography_service/src/services/berelekamp_massey.rs
+++ src/backend/steganography_service/src/services/berelekamp_massey.rs
@@ -1,9 +1,9 @@
 use crate::{
     errors::steg_service_error::StegServiceError,
-    services::galois_field::{poly_div, poly_mult, poly_mult_vecs},
+    services::galois_field::{poly_div, poly_mult},
 };
 
-pub fn berlecamp_massey(syndromes: &Vec<u8>) -> Result<(), StegServiceError> {
+pub fn berlecamp_massey(syndromes: &Vec<u8>) -> Result<Vec<u8>, StegServiceError> {
     let mut lambda = vec![1u8]; //Λ
     let mut prev_lambda = vec![1u8]; //B
     let mut prev_delta: u8 = 1; //b
@@ -16,19 +16,18 @@ pub fn berlecamp_massey(syndromes: &Vec<u8>) -> Result<(), StegServiceError> {
 
     let mut err_count: u8 = 0; //L
 
-    let mut i = 0;
-    loop {
+    for i in 0..syndromes.len() {
         shift += 1;
         let mut delta = 0;
 
         for j in 0..i + 1 {
-            // S[0] - Λ[1]*S[-1] = 0?
+            // S[0] - Λ[1]*S[-1] == 0?
             // we need to handle cases of out of bounds access
-            let coeff = match lambda.get(j + 1) {
+            let coeff = match lambda.get(j) {
                 Some(v) => v.clone(),
                 None => 0,
             };
-            let syndrome = match syndromes.get(j + 1) {
+            let syndrome = match syndromes.get(i - j) {
                 Some(v) => v.clone(),
                 None => 0,
             };
@@ -40,37 +39,39 @@ pub fn berlecamp_massey(syndromes: &Vec<u8>) -> Result<(), StegServiceError> {
         }
 
         // X^shift * B = adjusted_B
-        let mut adjusted_prev_lambda = vec![0u8, shift];
-        adjusted_prev_lambda.append(&mut prev_lambda);
+        let mut adjusted_prev_lambda = vec![0u8; shift as usize];
+        adjusted_prev_lambda.extend_from_slice(&prev_lambda);
 
-        prev_lambda = lambda.clone();
+        //we do this check here to save allocating memory to a temp vec which isnt that
+        //cheap. and we do this workaround with the delta as we still need the old value but
+        //cloning a number is cheap so we're fine with it
+        let prev_delta_to_use = prev_delta;
+        if err_count * 2 < i as u8 + 1 {
+            prev_lambda = lambda.clone();
+            prev_delta = delta;
+            err_count = i as u8 + 1 - err_count;
+
+            if err_count > (syndromes.len() / 2) as u8 {
+                return Err(StegServiceError::ReedSolomonError(
+                    "Too many errors to fix.".to_string(),
+                ));
+            }
+        }
 
         // δ/b * adjusted_B = adjusted_scaled_B
         for coeff in adjusted_prev_lambda.iter_mut() {
-            let multiplier = poly_div(delta, prev_delta);
+            let multiplier = poly_div(delta, prev_delta_to_use);
             *coeff = poly_mult(*coeff, multiplier);
         }
 
         //  Λ_old - adjusted_scaled_B = Λ_new
-        let len = if adjusted_prev_lambda.len() > lambda.len() {
-            adjusted_prev_lambda.len()
-        } else {
-            lambda.len()
-        };
-        for j in 0..len {
-            let curr_lambda_val = match lambda.get(j) {
-                Some(v) => v.clone(),
-                None => 0,
-            };
-            let prev_lambda_val = match adjusted_prev_lambda.get(j) {
-                Some(v) => v.clone(),
-                None => 0,
-            };
+        if lambda.len() < adjusted_prev_lambda.len() {
+            lambda.resize(adjusted_prev_lambda.len(), 0u8);
         }
-
-        i += 1;
-        break;
+        for j in 0..adjusted_prev_lambda.len() {
+            lambda[j] ^= adjusted_prev_lambda[j];
+        }
     }
 
-    todo!()
+    Ok(lambda)
 }
